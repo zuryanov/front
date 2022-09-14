3465965dbd2375fd328113d29e83b9725b45488f
ZyryanovAD <zyryanovad@skblab.ru>
2022-09-15 04:46:19 +0500

    Добавление пустых компонента и сервиса categories. Декомпозиция html-шаблона

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
